a0e40fe4611f47d7c230823ede3f76e98f42575d
roleyder <roggergarciadiaz@gmail.com>
2022-03-06 07:50:50 -0600

    Product Grid Colored

--- CapaPresentacion/frmIndicaProducto.cs
+++ CapaPresentacion/frmIndicaProducto.cs
@@ -26,17 +26,14 @@ namespace CapaPresentacion
                 dgvProduct.Columns.Add("ColumnId", "Id");
                 dgvProduct.Columns.Add("ColumnCodigo", "Code");
                 dgvProduct.Columns.Add("ColumnNombre", "Name");
-                dgvProduct.Columns.Add("ColumnPrecioCompra", "Purchase Price$");
+                dgvProduct.Columns.Add("ColumnPrecioCompra", "Purchase Price $");
                 dgvProduct.Columns.Add("ColumnPrecio", "Sale Price $");
                 dgvProduct.Columns.Add("ColumnStock", "Stock");
                 dgvProduct.Columns.Add("ColumnFechVen", "Due Date");
                 dgvProduct.Columns.Add("ColumnUnidMedida", "Category");
-                dgvProduct.Columns.Add("ColumnMateridida", "Prod. Present.");
-      
-                DataGridViewImageColumn dgvImagen = new DataGridViewImageColumn();
-                dgvImagen.HeaderText = "Status";
-                dgvImagen.Name = "ColumnEstado";
-                dgvProduct.Columns.Add(dgvImagen);
+                dgvProduct.Columns.Add("ColumnMateridida", "UM");
+                dgvProduct.Columns.Add("ColumnStatus", "Status");
+
 
                 dgvProduct.Columns[0].Visible = false;
                 dgvProduct.Columns[2].Width = 300;
@@ -45,7 +42,7 @@ namespace CapaPresentacion
                 dgvProduct.Columns[5].Width = 70;
                 dgvProduct.Columns[6].Width = 200;
                 dgvProduct.Columns[7].Width = 100;
-                dgvProduct.Columns[8].Width = 50;
+                dgvProduct.Columns[8].Width = 120;
                 dgvProduct.Columns[9].Width = 120;
               
                 DataGridViewCellStyle css = new DataGridViewCellStyle();
@@ -54,7 +51,7 @@ namespace CapaPresentacion
 
                 dgvProduct.AllowUserToAddRows = false;
                 dgvProduct.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-                dgvProduct.AllowUserToResizeColumns = false;
+                dgvProduct.AllowUserToResizeColumns = true;
 
             }
             catch (Exception) { throw; }
@@ -71,6 +68,7 @@ namespace CapaPresentacion
             }
             catch (Exception) { throw; }
         }
+       
         private void FillProductGrid(String name)
         {
             try
@@ -82,7 +80,7 @@ namespace CapaPresentacion
                 }
                 if (!String.IsNullOrEmpty(Convert.ToString(cboCategory.SelectedValue))) cat = (int)cboCategory.SelectedValue;
                 dgvProduct.Rows.Clear();
-                int rango = 0; Image img = null;
+                int rango = 0; String img = null;
                 if (rbStock.Checked == true) rango = 3;
                 else if (rbStockPro.Checked == true) rango = 2;
                 else if (rbStockMin.Checked == true) rango =1;
@@ -94,50 +92,30 @@ namespace CapaPresentacion
                     else { Lista = IBusinessManagement.Instancia.BuscarprodAvanzadaIndicador(name);}
                     for (int i =0; i <Lista.Count; i++)
                     {
-                        if (Lista[i].Stock_Prod >= 0 && Lista[i].Stock_Prod <= Lista[i].StockMin_Prod)
-                        {
-                            img = Properties.Resources.circulorojo_24x24;
-                        }
-                        else if (Lista[i].Stock_Prod > Lista[i].StockMin_Prod && Lista[i].Stock_Prod <= Lista[i].StockProm_Prod)
-                        {
-                            img = Properties.Resources.CirculoNaranja24x24;
-                        }
-                        else if (Lista[i].Stock_Prod  > Lista[i].StockProm_Prod)
-                        {
-                            img = Properties.Resources.circulo_verde24x24;
-                        }
+                    if (Lista[i].Stock_Prod >= 0 && Lista[i].Stock_Prod <= Lista[i].StockMin_Prod)
+                    {
+                        img = "Minimun";
+                        
+                    }
+                    else if (Lista[i].Stock_Prod > Lista[i].StockMin_Prod && Lista[i].Stock_Prod <= Lista[i].StockProm_Prod)
+                    {
+                        img = "Avarage";
+                        
+                    }
+                    else if (Lista[i].Stock_Prod > Lista[i].StockProm_Prod)
+                    {
 
-                       //DateTime DT = DateTime.TryParseExact(Lista[i].FechVen_Pord.ToString(), "dd/MM/yyyy", System.Globalization.CultureInfo.CurrentUICulture.DateTimeFormat);
-                        //DateTime date1l; String date2;
-                        //date1l = Convert.ToDateTime(Lista[i].FechVen_Pord.ToString());
-
-                        //if (DT < DateTime.Today)
-                        //{
-                        //    DataGridViewCellStyle css = new DataGridViewCellStyle();
-                        //    css.BackColor = Color.Red;
-                        //    dgvproducto.RowsDefaultCellStyle = css;
-                        //}
+                        img = "Enough";
+                    }
 
                         String[] fila = new String[] {
                             Lista[i].Id_Prod.ToString(), Lista[i].Codigo_Prod, Lista[i].Nombre_Prod, Lista[i].PrecioCompra_Prod.ToString(), Lista[i].Precio_Prod.ToString(),
                             Lista[i].Stock_Prod.ToString(),Lista[i].FechVen_Pord.ToString(), Lista[i].categoria.Nombre_Cat, Lista[i].unidmedida.Abreviatura_Umed };
                             dgvProduct.Rows.Add(fila);
                             dgvProduct.Rows[i].Cells[9].Value = img;
-                        
-                        //    DateTime date = Convert.ToDateTime(Lista[i].FechVen_Pord.ToString(), System.Globalization.CultureInfo.GetCultureInfo("hi-IN").DateTimeFormat);
-
-
-                        //if ( date < DateTime.Today)
-                        //{
-                        //    DataGridViewCellStyle css = new DataGridViewCellStyle();
-                        //    css.BackColor = Color.Red;
-                        //    dgvproducto.RowsDefaultCellStyle = css;
-                        //}
+   
                        }
                     }
-
-                                   
-                
             
             catch (Exception) { throw; }
         }
@@ -153,6 +131,7 @@ namespace CapaPresentacion
             {
                 BuildProductGrid();
                 FillComboBox();
+                
             }
             catch (ApplicationException es)
             {
@@ -165,12 +144,37 @@ namespace CapaPresentacion
                   "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             } 
         }
+        private void addDataGridViewCellColors()
+        {
+           
+            foreach (DataGridViewRow dgvr in dgvProduct.Rows)
+            {
 
+              
+                if (dgvr.Cells["ColumnStatus"].Value.ToString() == "Enough")
+                {
+                    dgvr.DefaultCellStyle.BackColor = Color.Green;
+                   
+                }
+
+                if (dgvr.Cells["ColumnStatus"].Value.ToString() == "Avarage")
+                {
+                    dgvr.DefaultCellStyle.BackColor = Color.Orange;
+                }
+
+                if (dgvr.Cells["ColumnStatus"].Value.ToString() == "Minimun")
+                {
+                    dgvr.DefaultCellStyle.BackColor = Color.Red;
+                }
+
+            }
+        }
         private void button1_Click(object sender, EventArgs e)
         {
             try
             {
                 FillProductGrid(null);
+                addDataGridViewCellColors();
             }
             catch (ApplicationException es)
             {
